36f3dacdba80793c2d219f6e38c916b42bc3316f
Chelsea Ndikum <46462443+AngelChels@users.noreply.github.com>
2024-09-17 08:59:13 +0000

    Initial Commit with database connection test

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const sql = require('mssql');
 require('dotenv').config();
 
 const config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
-    database: process.env.DB_DATABASE,
+    user: process.env.Username,
+    password: process.env.Pa$$w0rd,
+    server: process.env.addie-azuredb.database.windows.net,
+    database: process.env.TmsDatabase,
     options: {
         encrypt: true,
         trustServerCertificate: true,
