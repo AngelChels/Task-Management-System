7d02bf58c56c4dea05ac0e25ff366bb8fac9a549
AdewaleOpeloyeru <Opeloyeruadewale@icloud.com>
2024-09-17 12:42:33 +0000

    added error callback in SQL server connection process

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Task-Management-System
 The project involves developing a basic Task Management System (TMS) that allows users to create, manage, and track their tasks. The project will be structured to allow a complete development cycle, from database design and implementation to backend and frontend development, ensuring all components work together seamlessly.
+
+# .env_sample
+1. Rename .env_sample to .env
+2. Replace the values for DB_USER, DB_PASSWORD, DB_SERVER and DB_DATABASE with your server and database credentials.
+3. To set up a free database in Microsoft azure:
+    * Create an account
+    * Follow this tutorial:                -----Link-----
--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const sql = require('mssql');
 require('dotenv').config();
 
-const config = {
-    user: process.env.DB_USER,
+//rename your .env_sample file to .env and fill in your database credentials
+//for further info: README.md # .env_sample
+const sqlConfig = {
+    user: process.env.DB_USER,                    
     password: process.env.DB_PASSWORD,
     server: process.env.DB_SERVER,
     database: process.env.DB_DATABASE,
@@ -12,15 +14,19 @@ const config = {
     },
 };
 
-const poolPromise = new sql.ConnectionPool(config)
-    .connect()
-    .then(pool => {
-        console.log('Connected to SQL Server');
-        return pool;
-    })
-    .catch(err => console.log('Database connection failed', err));
 
-module.exports = {
-    sql,
-    poolPromise
-};
+
+(async () => {
+    try {
+     // make sure that any items are correctly URL encoded in the connection string
+     await sql.connect(sqlConfig);
+        const result = await (sql.query`select * from Tms_user`).then(
+            () => {console.log('Connected to SQL Server');}, 
+            () => {console.log('SQL server connection failed');});
+            //console.dir(result)
+     
+    } catch (err) {
+     // ... error checks
+     console.log('Database connection failed', err)
+    }
+   })();
